389b82d91bef5115946b63cd5a2f1e8d4235e0a5
Angelina Robin <angelina.robin@experionglobal.local>
2023-11-09 17:31:33 +0530

    fix:- Fixed the cards by adding less dialogue.

--- Support/js/support-1.js
+++ Support/js/support-1.js
@@ -1,15 +1,15 @@
 async function func2(Card, divRow) {
   try {
     const request = await fetch(
-      "https://dummyjson.com/posts?limit=30&skip=10&select=title,body"
+      "https://dummyjson.com/comments?limit=10&skip=10&select=body,username"
     );
     let data = await request.json();
     console.log(data);
 
-    console.log(divRow);
-    for (let index = 0; index < data.posts.length; index++) {
-      let post = data.posts[index];
-      create(post, Card);
+    // console.log(divRow);
+    for (let index = 0; index < data.comments.length; index++) {
+      let comment = data.comments[index];
+      create(comment, Card);
       divRow.appendChild(Card);
       return;
     }
@@ -18,7 +18,7 @@ async function func2(Card, divRow) {
   }
 }
 
-function create(post, Card) {
+function create(comment, Card) {
   const cardText = document.createElement("div");
   cardText.className = "card-text";
   const cardLink = document.createElement("a");
@@ -26,10 +26,10 @@ function create(post, Card) {
   cardLink.className = "card-link";
   const cardTitle = document.createElement("h3");
   cardTitle.className = "card-text-title";
-  cardTitle.textContent = post.title;
+  cardTitle.textContent = comment.user.username;
   const cardDesc = document.createElement("p");
   cardDesc.className = "card-text-description";
-  cardDesc.textContent = post.body;
+  cardDesc.textContent = comment.body;
   cardLink.appendChild(cardTitle);
   cardText.appendChild(cardLink);
   cardText.appendChild(cardDesc);
