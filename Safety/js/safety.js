a0cfb722163608c8e4237e1ada60c918965ca3b3
Gopika G Krishnan <gopika.krishnan@experionglobal.local>
2023-11-11 08:48:07 +0530

    fix:- test1

---
+++ Safety/js/safety.js
@@ -0,0 +1,108 @@
+const url="https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=6"
+ 
+ 
+ 
+async function getData(url){
+    try{
+        const response = await fetch(url,{
+            method:'get',
+            });
+            if(!response.ok){
+                throw new Error("network response was not ok");
+            }
+            const responseData=await response.json();
+ 
+            console.log(responseData);
+            let safetyDetailTag=document.getElementsByClassName("safety-detail");
+    for(let i=0;i<6;i++)
+    {
+        if(i%2!=0)
+        {
+    let safetyDetailDiv=document.createElement('div');
+    safetyDetailDiv.classList.add("safety-detail-content");
+    let safetyDetailTextDiv=document.createElement('div');
+    safetyDetailTextDiv.classList.add("safety-detail-text");
+    let safetyDetailHeadingDiv=document.createElement('h2');
+    safetyDetailHeadingDiv.classList.add("safety-detail-heading");
+    safetyDetailHeadingDiv.textContent= responseData.photos[i].title;
+    safetyDetailTextDiv.appendChild(safetyDetailHeadingDiv);
+    let safetyDetailParagraphDiv=document.createElement('div');
+    safetyDetailParagraphDiv.classList.add("safety-detail-paragraph");
+    let safetyDetailParaDiv=document.createElement('p');
+    safetyDetailParaDiv.textContent=responseData.photos[i].description;
+    safetyDetailParagraphDiv.appendChild(safetyDetailParaDiv);
+    safetyDetailTextDiv.appendChild(safetyDetailParagraphDiv);
+    let safetyDetailExploreMoreDiv=document.createElement('div');
+    safetyDetailExploreMoreDiv.classList.add("safety-detail-explore-more");
+    let safetyDetailExploreMoreTextDiv=document.createElement('div');
+    safetyDetailExploreMoreTextDiv.classList.add("safety-detail-explore-more-text");
+    let safetyDetailHeadingExploreMoreDiv=document.createElement('h4');
+    safetyDetailHeadingExploreMoreDiv.textContent="Explore more";
+    safetyDetailExploreMoreTextDiv.appendChild(safetyDetailHeadingExploreMoreDiv);
+    safetyDetailExploreMoreDiv.appendChild(safetyDetailExploreMoreTextDiv);
+    var safetyDetailArrowDiv=document.createElement('div');
+    safetyDetailArrowDiv.classList.add("arrow");
+    var img=document.createElement('img');
+    img.setAttribute('src',"/Discord-Cloning/Safety/images/arrow.png");
+    safetyDetailArrowDiv.appendChild(img);
+    safetyDetailExploreMoreDiv.appendChild(safetyDetailArrowDiv);
+    safetyDetailTextDiv.appendChild(safetyDetailExploreMoreDiv);
+    safetyDetailDiv.appendChild(safetyDetailTextDiv);
+    // image
+    let safetyDetailImageDiv=document.createElement('div');
+    safetyDetailImageDiv.classList.add("safety-detail-image");
+    var safetyDetailImage=document.createElement('img');
+    safetyDetailImage.setAttribute("src",responseData.photos[i].url);
+    safetyDetailImageDiv.appendChild(safetyDetailImage);
+    safetyDetailDiv.appendChild(safetyDetailImageDiv);
+    safetyDetailTag[0].appendChild(safetyDetailDiv);
+    }
+    else
+    {
+    let safetyDetailDiv=document.createElement('div');
+    safetyDetailDiv.classList.add("safety-detail-content");
+         // image
+    let safetyDetailImageDiv=document.createElement('div');
+    safetyDetailImageDiv.classList.add("safety-detail-image");
+    var safetyDetailImage=document.createElement('img');
+    safetyDetailImage.setAttribute("src",responseData.photos[i].url);
+    safetyDetailImageDiv.appendChild(safetyDetailImage);
+    safetyDetailDiv.appendChild(safetyDetailImageDiv);
+    
+    
+    let safetyDetailTextDiv=document.createElement('div');
+    safetyDetailTextDiv.classList.add("safety-detail-text");
+    let safetyDetailHeadingDiv=document.createElement('h2');
+    safetyDetailHeadingDiv.classList.add("safety-detail-heading");
+    safetyDetailHeadingDiv.textContent= responseData.photos[i].title;
+    safetyDetailTextDiv.appendChild(safetyDetailHeadingDiv);
+    let safetyDetailParagraphDiv=document.createElement('div');
+    safetyDetailParagraphDiv.classList.add("safety-detail-paragraph");
+    let safetyDetailParaDiv=document.createElement('p');
+    safetyDetailParaDiv.textContent=responseData.photos[i].description;
+    safetyDetailParagraphDiv.appendChild(safetyDetailParaDiv);
+    safetyDetailTextDiv.appendChild(safetyDetailParagraphDiv);
+    let safetyDetailExploreMoreDiv=document.createElement('div');
+    safetyDetailExploreMoreDiv.classList.add("safety-detail-explore-more");
+    let safetyDetailExploreMoreTextDiv=document.createElement('div');
+    safetyDetailExploreMoreTextDiv.classList.add("safety-detail-explore-more-text");
+    let safetyDetailHeadingExploreMoreDiv=document.createElement('h4');
+    safetyDetailHeadingExploreMoreDiv.textContent="Explore more";
+    safetyDetailExploreMoreTextDiv.appendChild(safetyDetailHeadingExploreMoreDiv);
+    safetyDetailExploreMoreDiv.appendChild(safetyDetailExploreMoreTextDiv);
+    var safetyDetailArrowDiv=document.createElement('div');
+    safetyDetailArrowDiv.classList.add("arrow");
+    var img=document.createElement('img');
+    img.setAttribute('src',"/Discord-Cloning/Safety/images/arrow.png");
+    safetyDetailArrowDiv.appendChild(img);
+    safetyDetailExploreMoreDiv.appendChild(safetyDetailArrowDiv);
+    safetyDetailTextDiv.appendChild(safetyDetailExploreMoreDiv);
+    safetyDetailDiv.appendChild(safetyDetailTextDiv);
+    safetyDetailTag[0].appendChild(safetyDetailDiv);
+    }}
+}
+catch(error){
+    console.error("there was a problem with fetch operation: ",error);
+}
+}
+getData(url)
