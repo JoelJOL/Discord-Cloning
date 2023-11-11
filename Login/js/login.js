9c7cc664cf7f492329d6345796b6bd46463d9167
Abraham Jacob Kurialanickal <abraham.k@experionglobal.local>
2023-11-11 09:44:50 +0530

    feat:- added validation

--- Login/js/login.js
+++ Login/js/login.js
@@ -17,23 +17,68 @@ getUserData
       users.push(element);
     });
   });
+console.log(users);
 
 function forgotUsername() {
   let resetEmail = prompt("Enter an email id to send username recovery link");
+  let validEmailExpr =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (resetEmail === "") {
+    alert("Email cannot be empty");
+  } else if (!resetEmail.match(validEmailExpr)) {
+    alert("Not a valid email address");
+  } else {
+    alert(
+      `An email with username recovery link has been sent to ${resetEmail}`
+    );
+  }
 }
 function forgotPassword() {
   let usernameField = document.getElementById("login-username");
   if (usernameField.value === "") {
-    alert("Username cannot be empty");
+    alert("Please enter username and try again");
+  } else {
+    if (isValidUsername(usernameField.value)) {
+      alert(
+        `An email with password recovery link has been sent to email id corresponding to ${usernameField.value}`
+      );
+    } else {
+      alert("Username not found");
+    }
   }
 }
 function validateLogin() {
   let usernameField = document.getElementById("login-username");
+  let passwordField = document.getElementById("login-password");
   if (usernameField.value === "") {
     alert("Username cannot be empty");
-  }
-  let passwordField = document.getElementById("login-password");
-  if (passwordField.value.length < 9) {
+  } else if (passwordField.value.length < 8) {
     alert("Password must be minimum 8 characters");
+  } else {
+    let userId = isValidLogin(usernameField.value, passwordField.value);
+    if (userId === -1) {
+      alert("User does not exist");
+    } else {
+      alert(`Welcome ${users[userId].firstName}`);
+    }
+  }
+}
+function isValidUsername(checkUsername) {
+  for (let i = 0; i < users.length; i++) {
+    if (checkUsername === users[i].username) {
+      return true;
+    }
+  }
+}
+function isValidLogin(checkUsername, checkPassword) {
+  for (let i = 0; i < users.length; i++) {
+    if (
+      checkUsername === users[i].username &&
+      checkPassword === users[i].password
+    ) {
+      return i;
+    } else {
+      return -1;
+    }
   }
 }
--- Login/js/register.js
+++ Login/js/register.js
@@ -12,7 +12,6 @@ const month = [
   "November",
   "December",
 ];
-
 for (let i = 0; i < month.length; i++) {
   let monthList = document.getElementById("month-list");
   let monthItem = document.createElement("option");
@@ -33,4 +32,30 @@ for (let i = 2020; i >= 1900; i--) {
   yearItem.setAttribute("value", `${i}`);
   yearItem.innerHTML = `${i}`;
   yearList.appendChild(yearItem);
+}
+
+function validateRegister() {
+  let emailField = document.getElementById("register-email");
+  if (emailField.value === "") {
+    alert("Email cannot be empty");
+  } else {
+    let validEmailExpr =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!emailField.value.match(validEmailExpr)) {
+      alert("Not a valid email address");
+    }
+  }
+  let usernameField = document.getElementById("register-username");
+  if (usernameField.value === "") {
+    alert("Username cannot be empty");
+  }
+  let passwordField = document.getElementById("register-password");
+  if (passwordField.value.length < 9) {
+    alert("Password must be minimum 8 characters");
+  }
+  let monthField = document.getElementById("month-list");
+  let dateField = document.getElementById("date-list");
+  let yearField = document.getElementById("year-list");
+  let date = `${yearField.value}/${monthField.value}/${dateField.value}`;
+  console.log(date);
 }
