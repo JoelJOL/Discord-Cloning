26f634982929e09ed3936923aec1450576740325
Abraham Jacob Kurialanickal <abraham.k@experionglobal.local>
2023-11-11 11:10:39 +0530

    feat:- added POST method

--- Login/js/register.js
+++ Login/js/register.js
@@ -36,26 +36,71 @@ for (let i = 2020; i >= 1900; i--) {
 
 function validateRegister() {
   let emailField = document.getElementById("register-email");
-  if (emailField.value === "") {
-    alert("Email cannot be empty");
-  } else {
-    let validEmailExpr =
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (!emailField.value.match(validEmailExpr)) {
-      alert("Not a valid email address");
-    }
-  }
+  let displaynameField = document.getElementById("register-name");
   let usernameField = document.getElementById("register-username");
-  if (usernameField.value === "") {
-    alert("Username cannot be empty");
-  }
   let passwordField = document.getElementById("register-password");
-  if (passwordField.value.length < 9) {
-    alert("Password must be minimum 8 characters");
-  }
   let monthField = document.getElementById("month-list");
   let dateField = document.getElementById("date-list");
   let yearField = document.getElementById("year-list");
   let date = `${yearField.value}/${monthField.value}/${dateField.value}`;
-  console.log(date);
+  let acceptTerms = document.getElementById("register-terms");
+  if (emailField.value === "") {
+    alert("Email cannot be empty");
+  } else if (!isValidEmail(emailField.value)) {
+  } else if (usernameField.value === "") {
+    alert("Username cannot be empty");
+  } else if (passwordField.value.length < 8) {
+    alert("Password must be minimum 8 characters");
+  } else if (
+    yearField.value === "" ||
+    monthField.value === "" ||
+    dateField.value === ""
+  ) {
+    alert("Invalid date");
+  } else if (!acceptTerms.checked) {
+    alert("Accept the Terms and Policy");
+  } else {
+    let userData = {
+      email: emailField.value,
+      username: usernameField.value,
+      firstName: displaynameField.value,
+      password: passwordField.value,
+      birthDate: date,
+    };
+    const postUrl = "https://dummyjson.com/users/add";
+    const postResponse = addUser(postUrl, userData);
+    console.log(postResponse);
+  }
+}
+
+function isValidEmail(registerEmail) {
+  let validEmailExpr =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!registerEmail.match(validEmailExpr)) {
+    alert("Not a valid email address");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// function isValidDate() {}
+
+async function addUser(url, data) {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
 }
