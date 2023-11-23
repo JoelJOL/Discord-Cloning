370866c7ff285d578bd87f05b86ffe5fba82956c
Varkey E Njaliath <varkey.njaliath@experionglobal.local>
2023-11-23 12:10:47 +0530

    feat:homepage refined

--- Homepage/JS/home.js
+++ Homepage/JS/home.js
@@ -1,11 +1,11 @@
 
-const url="https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=6"
+const url1="https://mocki.io/v1/89d8db87-1e7d-4756-a1d7-908747f7049d"
 
 
 
-async function getData(url){
+async function getData(url1){
     try{
-        const response = await fetch(url,{
+        const response = await fetch(url1,{
             method:'get',
             });
             if(!response.ok){
@@ -16,7 +16,7 @@ async function getData(url){
             console.log(responseData);
 
                 let mainSection=document.getElementsByClassName("dynamicbox");
-            for(i=0;i<5;i++)
+            for(i=0;i<3;i++)
             {
                 let row=document.createElement('div');
                 row.classList.add("container");
@@ -24,6 +24,7 @@ async function getData(url){
                 let rowimage=document.createElement('div');
                 rowimage.classList.add("container-img");``
                 let dynamicImage = document.createElement('img'); 
+                dynamicImage.classList.add("home-feed-images");
                 dynamicImage.setAttribute("src",responseData.photos[i].url);
         
 
@@ -63,4 +64,52 @@ async function getData(url){
             console.error("there was a problem with fetch operation: ",error);
         }
     }
-getData(url)
+
+getData(url1);
+
+
+//////////////////////////////////////////////////////
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Add an event listener to the button
+    document.getElementById('opendis').addEventListener('click', function () {
+      // Replace the button with a textbox
+      replaceWithTextbox(this);
+  
+      // Hide the "Download for Windows" button
+      document.getElementById('dfw').style.display = 'none';
+    });
+  
+    // Function to replace the button with a textbox
+    function replaceWithTextbox(button) {
+      var input = document.createElement('input');
+      input.type = 'text';
+      input.placeholder = 'Enter Discord URL';
+  
+      // Insert the input after the button
+      button.parentNode.insertBefore(input, button.nextSibling);
+  
+      // Hide the button
+      button.style.display = 'none';
+  
+      // Focus on the input
+      input.focus();
+  
+      // Add a blur event listener to switch back to the button when the input loses focus
+      input.addEventListener('blur', function () {
+        // Remove the input
+        input.parentNode.removeChild(input);
+  
+        // Show the button again
+        button.style.display = 'inline-block';
+  
+        // Show the "Download for Windows" button
+        document.getElementById('dfw').style.display = 'inline-block';
+      });
+    }
+  });
+  
+  /////////////////////////////////////////////////
+
+
